1b0cbb25f81668ad1a6c04a94796d4a9c1332759
Bensemmane Camélia <bensemmanecamelia@gmail.com>
2026-05-18 16:15:18 +0200

    logo + formulaire + nav

--- static/nav_membre.php
+++ static/nav_membre.php
@@ -2,6 +2,7 @@
     <ul>
         <li><a class="Accueuil" href="index.php">Accueuil</a></li>
         <li><a class="Déconnexion" href="index.php">Déconnexion</a></li>
+        <li><a class="Profil" href="profil.php">Profil</a></li>
     </ul>
     
 </nav>
