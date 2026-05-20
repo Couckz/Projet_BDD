3cfba982d8e4ece9c84f0f0facf06aba5473c35d
AlexG345 <alexgernez1@gmail.com>
2026-05-20 11:05:20 +0200

    changement accueil (balise a sur tout l'article et pas que l'mage

--- php/functions-structures.php
+++ php/functions-structures.php
@@ -17,48 +17,49 @@ function affichage_articles($articles){
             $synopsis = $lines["synopsis"];
 
             echo "<div class = 'border_article'>";
-                echo "<div class = 'presentation'>";
+                echo "<a href = article.php/?id_article=$id_article>";
 
-                    echo "<div class = 'intro'>";
-                        echo "<h2 class = 'title'>$titre</h2>";
-                        echo "<section class = 'caracteristique'>";
+                    echo "<div class = 'presentation'>";
 
-                            echo "<div class = 'tags_container'>";
-                                foreach($caracteristiques as $caracteristique){
-                                    echo "<div class = 'tag_bubble'>$caracteristique</div>";
-                                }
-                            echo "</div>";
+                        echo "<div class = 'intro'>";
+                            echo "<h2 class = 'title'>$titre</h2>";
+                            echo "<section class = 'caracteristique'>";
 
-                            echo "$synopsis<br/>";
-                        echo "</section>";
-                    echo "</div>";
+                                echo "<div class = 'tags_container'>";
+                                    foreach($caracteristiques as $caracteristique){
+                                        echo "<div class = 'tag_bubble'>$caracteristique</div>";
+                                    }
+                                echo "</div>";
 
-                    echo "<div class = 'intro2'>";
-                        echo "<div class = 'img_container'>";
-                            echo "<a href = article.php/?id_article=$id_article>";
+                                echo "$synopsis<br/>";
+                            echo "</section>";
+                        echo "</div>";
+
+                        echo "<div class = 'intro2'>";
+                            echo "<div class = 'img_container'>";
                                 $chemin_img = dirname($_SERVER['SCRIPT_NAME'])."/".$chemin_img;
                                 echo "<img src='$chemin_img'>";
-                            echo "</a>";
+                            echo "</div>";
+                            echo "<p>Prix : $prix</p>";
+                            echo "<p class='note'> Note : $note/10</p>";
                         echo "</div>";
-                        echo "<p>Prix : $prix</p>";
-                        echo "<p class='note'> Note : $note/10</p>";
                     echo "</div>";
 
+                    echo "<div class='contenu'>";
+                        echo "<p class='texte'>";
+                            echo $contenu;
+                        echo "<p>";
+                    echo "</div>";
 
-                echo "</div>";
+                    echo "<footer class = 'fin'>";
+                        echo "Date de création : $date_creation  ";
+                        echo "  Modifié le : $date_modification";
+                    echo "</footer>";
 
-                echo "<div class='contenu'>";
-                    echo "<p class='texte'>";
-                        echo $contenu;
-                    echo "<p>";
-                echo "</div>";
-
-                echo "<footer class = 'fin'>";
-                    echo "Date de création : $date_creation  ";
-                    echo "  Modifié le : $date_modification";
-                echo "</footer>";
+                echo "</a>";
 
             echo "</div>";
+
     }
 
     echo "</div>";
