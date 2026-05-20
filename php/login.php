da5f8a6ef6672343a527c29027d500f269256676
Bensemmane Camélia <bensemmanecamelia@gmail.com>
2026-05-20 08:16:22 +0200

    correction dossier

--- php/login.php
+++ php/login.php
@@ -6,13 +6,13 @@ function login()
     $connect = connection($sql_connection, $_POST['login'], $_POST['mdp']);
     if(empty($connect)){
         closeDB($sql_connection);
-        header("Location: http://localhost/Projet_BDD/connexion.php");
+        header("Location: ../Projet_BDD/connexion.php");
     }else{
         $_SESSION['login'] = $_POST['login'];
         $_SESSION['mdp'] = $_POST['mdp'];
         $_SESSION['connecte'] = true;
         closeDB($sql_connection);
-        header("Location: http://localhost/Projet_BDD/index.php");
+        header("Location: ../Projet_BDD/index.php");
     }
 }
 
