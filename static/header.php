a4477b391f1671552341b275bbbc24e220fd77a1
Bensemmane Camélia <bensemmanecamelia@gmail.com>
2026-05-20 10:31:21 +0200

    mes sauvegardes

--- static/header.php
+++ static/header.php
@@ -5,7 +5,7 @@
     <a href="index.php">
         <?php
         $rootPath = '/' . explode('/', trim($_SERVER['SCRIPT_NAME'], '/'))[0];
-        echo "<img src='$rootPath/img/site/logo.png' alt='Logo du site'>"
+        echo "<img src='$rootPath/Projet_BDD/img/site/logo.png' alt='Logo du site'>"
         ?>
     </a>
     <h1>Game-Actu</h1>
