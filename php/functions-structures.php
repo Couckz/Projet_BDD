e97caa64feed9f22d1f052ba45e12588f40d3fd3
AlexG345 <alexgernez1@gmail.com>
2026-05-20 09:21:41 +0200

    function_structure fix

--- php/functions-structures.php
+++ php/functions-structures.php
@@ -63,7 +63,6 @@ function affichage_articles($articles){
     echo "</div>";
 }
 
-<<<<<<< Updated upstream
 function date_to_str($date){
     $chaine_date = substr($date, 8, 2);
     switch(substr($date, 5, 2)){
@@ -110,64 +109,6 @@ function date_to_str($date){
     $chaine_date = $chaine_date.substr($date, 0, 4);
     return $chaine_date;
 }
-=======
-
-function affichage_article($article) {
-
-    $id_article = $article["id_article"];
-    $titre = $article["titre"];
-    $contenu = $article["contenu"];
-    $date_creation = $article["date_creation"];
-    $date_modification = $article["date_modification"];
-    $note = $article["note"];
-    $caracteristiques = explode(",", $article["caracteristiques"]);
-    $chemin_img = $article["chemin_image"];
-    $prix = $article["prix"];
-    $synopsis = $article["synopsis"];
-
-    echo "<div class = 'border_article'>";
-        echo "<div class = 'presentation'>";
-
-            echo "<div class = 'intro'>";
-                echo "<h2 class = 'title'>$titre</h2>";
-                echo "<section class = 'caracteristique'>";
-
-                    echo "<div class = 'tags_container'>";
-                        foreach($caracteristiques as $caracteristique){
-                            echo "<div class = 'tag_bubble'>$caracteristique</div>";
-                        }
-                    echo "</div>";
-
-                    echo "$synopsis<br/>";
-                echo "</section>";
-            echo "</div>";
-
-            echo "<div class = 'intro2'>";
-                echo "<div class = 'img_container'>";
-                    echo "<img src='../$chemin_img'>";
-                echo "</div>";
-                echo "<p>Prix : $prix</p>";
-                echo "<p class='note'> Note : $note/10</p>";
-            echo "</div>";
-
-
-            echo "</div>";
-
-        echo "<div class='contenu'>";
-            echo "<p class='texte'>";
-                echo $contenu;
-            echo "<p>";
-        echo "</div>";
-
-        echo "<footer class = 'fin'>";
-            echo "Date de création : $date_creation  ";
-            echo "  Modifié le : $date_modification";
-        echo "</footer>";
-
-    echo "</div>";
-}
-
->>>>>>> Stashed changes
 function affichage_article($article) {
 
     $id_article = $article["id_article"];
