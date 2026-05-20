28b6da7abce8423fb2ba32032a9894c08e2a3064
AlexG345 <alexgernez1@gmail.com>
2026-05-20 09:08:30 +0200

    debut page article

---
+++ article.php
@@ -0,0 +1,51 @@
+<?php
+ini_set('display_errors', 1); //active l'affichage des erreurs
+ini_set('display_startup_errors', 1); //gestion des affichages d'erreur au démarage
+error_reporting(E_ALL); //affiche toute les erreurs possible
+
+if (!isset($_GET["id_article"])) {
+    header("Location: ../index.php");
+}
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //déclenche une erreur PHP dés qu'une requête échoue, puis transformes ces erreurs en exception
+require_once("./includes/constantes.php"); //constantes du site
+require_once("./includes/config-bdd.php"); //donnees pour la connexion
+require_once("./php/functions-DB.php"); //functions de connexions
+require_once("./php/functions-query.php"); //functions de requêtes SQL
+require_once("./php/functions-structures.php"); //functions de mises en pages au niveau des données obtenues de la BDD
+$mysqli = connectionDB(); //création de la connexion SQL
+?>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title><?php echo "$titreSite"; ?></title>
+        <meta charset="utf-8">
+        <meta name="keywords" content="jeu vidéo article">
+        <meta name="author" content="Malo Camelia Alexandre">
+        <link rel="icon" href="../images/pokeball.png">
+        <link rel="stylesheet" type="text/css" href="../styles/style.css">
+    </head>
+    <body>
+        <?php
+            include("static/header.php"); //inclusion du header de la page
+            include("static/nav.php"); //inclusion de l'onglet navigation de la page
+        ?>
+        <main>
+            <?php
+
+            $id_article = htmlspecialchars($_GET["id_article"]);
+            $article = information_article($mysqli, $id_article); //on récupère un article de la BDD identifié par son ID
+
+            if (!isset($article[0])) {
+                header("Location: ../index.php");
+            }
+
+            affichage_article($article[0]); //on affiche simplement l'article
+            ?>
+        </main>
+        <?php
+            include("static/footer.php"); //inclusion du footer de la page
+            closeDB($mysqli); //cloture de la connexion SQL
+        ?>
+    </body>
+</html>
