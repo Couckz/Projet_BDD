20c492bf1ec9b85a3269f8066ea011566afd5bd0
Malo <malo.boudieraebischer@etudiant.univ-rennes.fr>
2026-05-20 11:03:39 +0200

    réglage du bug du nav

--- static/nav.php
+++ static/nav.php
@@ -7,8 +7,8 @@ $rootPath = dirname($_SERVER['SCRIPT_NAME']);
         <li><a href="index.php">Accueil</a></li>
         <?php
             if(isset($_SESSION['connecte']) && $_SESSION['connecte']){
-                ?><li><a href="profil.php">Profil</a></li><?php
-                ?><li><a href="php/logout.php">Déconnexion</a></li><?php
+                ?><li><a href="<?php echo $rootPath; ?>/profil.php">Profil</a></li><?php
+                ?><li><a href="<?php echo $rootPath; ?>/php/logout.php">Déconnexion</a></li><?php
             }else{
                 ?><li><a href="<?php echo $rootPath; ?>/connexion.php">Connexion</a></li><?php
             }?>
