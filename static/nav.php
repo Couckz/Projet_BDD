6ba7e8a6c3f2306a6f30f0f6c20f0f16633ee3a5
Malo <malo.boudieraebischer@etudiant.univ-rennes.fr>
2026-05-20 11:00:06 +0200

    image de profile uniformisé

--- static/nav.php
+++ static/nav.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$rootPath = dirname($_SERVER['SCRIPT_NAME']);
 ?>
 <nav>
     <ul>
@@ -9,7 +10,7 @@ session_start();
                 ?><li><a href="profil.php">Profil</a></li><?php
                 ?><li><a href="php/logout.php">Déconnexion</a></li><?php
             }else{
-                ?><li><a href="connexion.php">Connexion</a></li><?php
+                ?><li><a href="<?php echo $rootPath; ?>/connexion.php">Connexion</a></li><?php
             }?>
     </ul>
 </nav>
