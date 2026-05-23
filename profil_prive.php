a821c5e59c975fa45510778b6e5f0a40da4c0420
AlexG345 <alexgernez1@gmail.com>
2026-05-24 00:06:32 +0200

    un peu de css profil

--- profil_prive.php
+++ profil_prive.php
@@ -46,55 +46,62 @@ if (isset($_POST['btn_date_naissance_modif'])) {
             include("static/nav.php"); //inclusion de l'onglet navigation de la page
         ?>
         <main>
-            <h2 class="salutation">Bonjour <?php echo $_SESSION['login'];?>!</h2>
-            <?php
-            if (isset($_POST['btnPhoto']) && !empty($_FILES['pp']['name'])) {
-                change_photo();
-            }
-            ?>
-            <form action="#" method="post" enctype="multipart/form-data">
-                <label for="inputPdp"  class="profile" style="cursor:pointer;">
-                    <img src="<?php echo $_SESSION['chemin_pdp']; ?>" alt="photo de profil">
-                </label>
-                <input type="file" id="inputPdp" name="pp" accept="image/*" style="display:none;">
-                <br><button type="submit" name="btnPhoto">Changer</button>
-                <p class="profile">
-                    <span>
-                        Membre depuis le : <?php echo date_to_str($_SESSION['date_inscription']); ?> 
-                    </span>
-                    <span>
-                        Identité : <?php echo $_SESSION['prenom'];echo " ". $_SESSION['nom']; ?> 
-                        <button type="submit" name="btn_nom">Changer</button><br>
-                        <?php
-                            if (isset($_POST['btn_nom'])){?>
-                                <input required type="text" name="nom" placeholder="ex: Chirac"><br>
-                                <input required type="text" name="prenom" placeholder="ex: Jacques"><br>
-                                <button type="submit" name="btn_nom_modif">Confirmer</button><?php
-                            }
-                        ?>
-                    </span>
-                    <span>
-                        Né le : <?php echo date_to_str($_SESSION['date_naissance']); ?> 
-                        <button type="submit" name="btn_date_naissance">Changer</button>
-                        <?php
-                            if (isset($_POST['btn_date_naissance'])){?>
-                                <input required type="date" name="date_naissance"><br>
-                                <button type="submit" name="btn_date_naissance_modif">Confirmer</button><?php
-                            }
-                        ?>
-                    </span>
-                    <span>
-                        Adresse e-mail : <?php echo $_SESSION['adresse_email']; ?> 
-                        <button type="submit" name="btn_adresse_email">Changer</button>
-                        <?php
-                            if (isset($_POST['btn_adresse_email'])){?>
-                                <input required type="email" name="adresse_email" placeholder="ex: david.goodenough@gmail.com"><br>
-                                <button type="submit" name="btn_adresse_email_modif">Confirmer</button><?php
-                            }
-                        ?>
-                    </span>
-                </p>
-            </form>
+            <div class = "profile">
+                <h1 class="salutation">Bonjour <?php echo $_SESSION['login'];?>!</h2>
+                <?php
+                if (isset($_POST['btnPhoto']) && !empty($_FILES['pp']['name'])) {
+                    change_photo();
+                }
+                ?>
+                <form action="#" method="post" enctype="multipart/form-data">
+
+                    <div>
+                        <label for="inputPdp" style="cursor:pointer;">
+                            <img src="<?php echo $_SESSION['chemin_pdp']; ?>" alt="photo de profil">
+                        </label>
+                        <input type="file" id="inputPdp" name="pp" accept="image/*" style="display:none;">
+                        <br><button class="modification-button" type="submit" name="btnPhoto">Changer</button>
+                    </div>
+
+                    <div class="profile-info">
+
+                        <span>
+                            <span class = "profile-info-label">Membre depuis le : </span><?php echo date_to_str($_SESSION['date_inscription']); ?>
+                        </span>
+                        <span>
+                            <span class = "profile-info-label">Identité : </span><?php echo $_SESSION['prenom'];echo " ". $_SESSION['nom']; ?>
+                            <button class="modification-button" type="submit" name="btn_nom">Changer</button><br>
+                            <?php
+                                if (isset($_POST['btn_nom'])){?>
+                                    <input required type="text" name="nom" placeholder="ex: Chirac"><br>
+                                    <input required type="text" name="prenom" placeholder="ex: Jacques"><br>
+                                    <button class="confirmation-button" type="submit" name="btn_nom_modif">Confirmer</button><?php
+                                }
+                            ?>
+                        </span>
+                        <span>
+                            <span class = "profile-info-label">Né le : </span><?php echo date_to_str($_SESSION['date_naissance']); ?>
+                            <button class="modification-button" type="submit" name="btn_date_naissance">Changer</button>
+                            <?php
+                                if (isset($_POST['btn_date_naissance'])){?>
+                                    <input required type="date" name="date_naissance"><br>
+                                    <button type="submit" name="btn_date_naissance_modif">Confirmer</button><?php
+                                }
+                            ?>
+                        </span>
+                        <span>
+                            <span class = "profile-info-label">Adresse e-mail : </span><?php echo $_SESSION['adresse_email']; ?>
+                            <button class="modification-button" type="submit" name="btn_adresse_email">Changer</button>
+                            <?php
+                                if (isset($_POST['btn_adresse_email'])){?>
+                                    <input required type="email" name="adresse_email" placeholder="ex: david.goodenough@gmail.com"><br>
+                                    <button type="submit" name="btn_adresse_email_modif">Confirmer</button><?php
+                                }
+                            ?>
+                        </span>
+                    </div>
+                </form>
+            </div>
         </main>
         <?php
             include("static/footer.php"); //inclusion du footer de la page
